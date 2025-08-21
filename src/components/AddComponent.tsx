e48eecade7cc81fc271ab26f3980055add9b86ed
Юсуф <itrexus69@gmail.com>
2025-08-22 01:05:01 +0300

    добавил addComponent (add-button и input)

---
+++ src/components/AddComponent.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AddInput from './AddInput';
+import AddButton from './AddButton';
+
+const AddComponent = () => {
+    return (
+      <div className="button_input_box">
+        <AddInput />
+        <AddButton />
+      </div>
+    );
+}
+
+export default AddComponent;
---
+++ src/components/AddInput.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddInput = () => {
+    return (
+        <div>
+            <input type="text" />
+        </div>
+    );
+}
+
+export default AddInput;
