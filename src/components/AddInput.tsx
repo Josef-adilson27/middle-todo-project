a2435bff039a9e76486cc9cafff25238bec3adb3
Юсуф <itrexus69@gmail.com>
2025-08-22 04:10:39 +0300

    добавил context api

--- src/components/AddInput.tsx
+++ src/components/AddInput.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
-
-const AddInput = () => {
-    return (
-        <div>
-            <input type="text" />
-        </div>
-    );
-}
+const AddInput = ({
+  inputValue,
+  setVasetInputValuelue,
+}: {
+  inputValue: string;
+  setVasetInputValuelue: (val: string) => void;
+}) => {
+  return (
+    <div>
+      <input
+        value={inputValue}
+        onChange={(e) => setVasetInputValuelue(e.target.value)}
+        type="text"
+      />
+    </div>
+  );
+};
 
 export default AddInput;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {StatesProvider} from './context/Context'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <StatesProvider>
+      <App />
+    </StatesProvider>
   </StrictMode>,
 )
